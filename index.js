dd532b7f1e46b8a9f0bdf5fc29cb232a68e7cc79
Olivia Auzenne <oauzenne@gmail.com>
2019-04-02 20:16:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const Var = "hi"
+
+
+document.querySelector("main#main").remove();
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerText = "Olivia is the champion!";
+newHeader.className = "victory";// Write your code here!// Write your code here!
